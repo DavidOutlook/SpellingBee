4fc0e0ad07fe6191333f4821fe8863b1022a3d9a
David Allen <davidallen55555@yahoo.com>
2021-12-08 09:31:22 -0700

    1st commit

--- SpellingBee.ps1
+++ SpellingBee.ps1
@@ -1,2 +1,23 @@
 # Kanban Board created in GitKraken
 Write-Host "I or we are going to make flashcards to help my son learn spelling words"
+
+# Step 1 - Import Data
+
+
+
+# https://adamtheautomator.com/read-host/
+# prompt user for string value = read-host
+# powershell prompt user for input
+
+
+$word = Read-Host -prompt 'Spell'
+if ($word) {
+    input -eq true | Write-Host 'Correct!'
+} elseif (-eq false) {
+    Write-Host 'Try again'
+}
+
+#LOOP BACK if incorrect, 
+do {
+    
+} until (-eq true)
