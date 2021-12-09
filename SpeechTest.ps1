9f52adf495eeca79a191a2b9f7c78608c707387c
David Allen <davidallen55555@yahoo.com>
2021-12-09 13:27:29 -0700

    Rename files

--- SpeechTest.ps1
+++ SpeechTest.ps1
@@ -5,7 +5,3 @@ new-speech 'Enter the number of events you like to review'
 $newest = (Read-Host)
 Get-EventLog -logname $logname -newest $newest
 
-new-speech 'I love to spell'
-$spell = (read-host)
-new-speech 'What would you like to spell'
-$newest = (Read-Host)
