8b17f5c47e92823d853d84487358b08a9307b4d6
David Allen <davidallen55555@yahoo.com>
2021-12-12 11:44:23 -0700

    1st commit

--- StopWatch.ps1
+++ StopWatch.ps1
@@ -9,3 +9,11 @@ $StopWatch.Stop()
 
 $StopWatch.Elapsed.TotalHours
 $StopWatch.Elapsed.TotalSeconds
+
+# https://gist.github.com/pcgeek86/336e08d1a09e3dd1a8f0a30a9fe61c8a 
+#### Perform a task on a timer (ie. every 5000 milliseconds)
+$Timer = [System.Timers.Timer]::new(5000)
+Register-ObjectEvent -InputObject $Timer -EventName Elapsed -Action {
+  Write-Host -ForegroundColor Blue -Object 'Timer elapsed! Doing some work.'
+}
+$Timer.Start()   
